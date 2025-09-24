46bd186318b735e75d2a9fcff3c8003a1201194b
Peter Andi <peter.andi@axoflow.com>
2025-09-24 13:48:46 +0200

    Add some things (ice-beta, jjgf, jj-todo)

---
+++ dot_config/private_fish/functions/jj-todo.fish
@@ -0,0 +1,4 @@
+function jj-todo -d "Show TODOs added since main"
+  # ignore-working-copy will not snapshot the working copy opn the execution of the command
+  jj diff --from main --ignore-working-copy | rg "^+.*(//.*TODO.*)" -r '$1' | sed 's/\\\'/\\\\\'/g'| xargs -I {} rg -F '{}'
+end
