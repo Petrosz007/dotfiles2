e1d1f3846e98e6935d1891384e72f244798d0090
Peter Andi <peter.andi@axoflow.com>
2026-06-09 13:11:57 +0200

    viddy watch jj log

--- dot_config/private_fish/functions/jj-wlog.fish
+++ dot_config/private_fish/functions/jj-wlog.fish
@@ -1,4 +1,5 @@
 function jj-wlog -d "Watch the jj log output"
   # ignore-working-copy will not snapshot the working copy opn the execution of the command
-  watch --color --no-title jj --ignore-working-copy log --color=always
+  viddy -n 1s --no-title --disable_auto_save \
+    jj --no-integrate-operation --ignore-working-copy log --color=always
 end
